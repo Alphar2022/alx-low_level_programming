d7116bea0c8c20f21c7030608940f4d62d48a3a7
Sheriff Babatunde Adeniyi <alphar2022@gmail.com>
2023-08-11 09:30:53 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Return:1
  */
 int main(void)
 {
-char ch[] = fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-write(2,  ch, sizeof(ch));
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
