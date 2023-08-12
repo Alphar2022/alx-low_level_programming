f6e76239e4ed5f3770ef72697d0e24ef5e7fb8b6
Sheriff Babatunde Adeniyi <alphar2022@gmail.com>
2023-08-11 19:28:56 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, ch, sizeof(ch));
-char ch[] = fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-write(2,  ch, sizeof(ch));
-return (1);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
